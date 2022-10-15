010d8ff4b0b2d7bee5dc611f64d360e9a6971b7a
James <james.nascimento@ufrpe.br>
2022-10-14 21:13:03 -0300

    Inserção de variavéis e métodos no projeto java-anatomia-classes.

---
+++ Java Beginners/Fundamentos de Java/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,25 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {        
+
+        //Declaração de variáveis de tipos diferentes.
+        int anoAtual = 2022;
+        boolean informacao = true;
+        String primeiroNome = "James";
+        String segundoNome = "Anderson";
+
+        //Atribuição do métoddo nomeCompleto a uma variavel do tipo String. 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        //Impressão do nome usando o símbolo + para concatenar.      
+        System.out.println("Olá " + nomeCompleto + ", seja bem-vindo!");
+    }
+
+    //Metodo estatico nomeCompleto.
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        //Retorno do método usando concatenação de Strings.
+        return primeiroNome.concat(" ").concat( segundoNome);
+    }
+    
+}
