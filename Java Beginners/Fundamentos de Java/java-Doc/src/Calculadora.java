583b8bb53a872d02f14313968d94a50a8411b3e9
James <james.nascimento@ufrpe.br>
2022-10-16 07:52:37 -0300

    Adição do projeto caluladora para ilustrar documentação.

---
+++ Java Beginners/Fundamentos de Java/java-Doc/src/Calculadora.java
@@ -0,0 +1,27 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros.
+ * <p>
+ * <b>Nota:<b> Leia atentamente a documentação desta classe 
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author James Anderson
+ * @version 1.0
+ * @sinse 16/10/2022
+ */
+ 
+ public class Calculadora {
+    
+    /**
+     * Este método é utilizado para somar dois números inteiros.
+     * 
+     * @param numeroUm este é o primeiro parametro do método.
+     * @param numeroDois este é o segundo parametro do método.
+     * @return int o resultado deste método é a soma dos dois números.
+    */
+    public int somar(int numeroUm, int numeroDois){
+
+        return (numeroUm + numeroDois);
+    }        
+    
+}
